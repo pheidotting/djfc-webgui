c36ab5a393ba448cd32ca2048b0d7cbb66d3eef9
Patrick Heidotting <patrick@lakedigital.nl>
2015-10-13 18:56:01 +0200

    dataservices nogmaals refactoren

--- src/main/webapp/js/commons/navRegister.js
+++ src/main/webapp/js/commons/navRegister.js
@@ -6,143 +6,68 @@ define([ "commons/3rdparty/log"],
             bepaalUrl: function(welkeFunctie){
                 var BASISURL_RELATIEBEHEER = '../dejonge/rest';
 
-                var INLOGGEN = 'INLOGGEN';
-                var INGELOGDE_GEBRUIKER = 'INGELOGDE_GEBRUIKER';
-                var UITLOGGEN = 'UITLOGGEN';
+                var vars = [{naam: 'INLOGGEN',                          url: BASISURL_RELATIEBEHEER + '/authorisatie/authorisatie/inloggen'},
+                            {naam: 'INGELOGDE_GEBRUIKER',               url: BASISURL_RELATIEBEHEER + '/authorisatie/authorisatie/ingelogdeGebruiker'},
+                            {naam: 'UITLOGGEN',                         url: BASISURL_RELATIEBEHEER + '/authorisatie/authorisatie/uitloggen'},
 
-                var LIJST_RELATIES = 'LIJST_RELATIES';
-                var LEES_RELATIE = 'LEES_RELATIE';
-                var OPSLAAN_RELATIE = 'OPSLAAN_RELATIE';
-                var VERWIJDER_RELATIE = 'VERWIJDER_RELATIE';
+                            {naam: 'LIJST_RELATIES',                    url: BASISURL_RELATIEBEHEER + '/medewerker/gebruiker/zoekOpNaamAdresOfPolisNummer'},
+                            {naam: 'LEES_RELATIE',                      url: BASISURL_RELATIEBEHEER + '/medewerker/gebruiker/lees'},
+                            {naam: 'OPSLAAN_RELATIE',                   url: BASISURL_RELATIEBEHEER + '/medewerker/gebruiker/opslaan'},
+                            {naam: 'VERWIJDER_RELATIE',                 url: BASISURL_RELATIEBEHEER + '/medewerker/gebruiker/verwijderen'},
 
-                var LIJST_BEDRIJVEN_BIJ_RELATIE = 'LIJST_BEDRIJVEN_BIJ_RELATIE';
-                var OPSLAAN_BEDRIJF = 'OPSLAAN_BEDRIJF';
-                var LEES_BEDRIJF = 'LEES_BEDRIJF';
+                            {naam: 'LIJST_BEDRIJVEN_BIJ_RELATIE',       url: BASISURL_RELATIEBEHEER + '/medewerker/bedrijf/lijst'},
+                            {naam: 'OPSLAAN_BEDRIJF',                   url: BASISURL_RELATIEBEHEER + '/medewerker/gebruiker/opslaanBedrijf'},
+                            {naam: 'LEES_BEDRIJF',                      url: BASISURL_RELATIEBEHEER + '/medewerker/bedrijf/lees'},
 
-                var LIJST_VERZEKERINGSMAATSCHAPPIJEN = 'LIJST_VERZEKERINGSMAATSCHAPPIJEN';
+                            {naam: 'LIJST_VERZEKERINGSMAATSCHAPPIJEN',  url: BASISURL_RELATIEBEHEER + '/medewerker/overig/lijstVerzekeringsMaatschappijen'},
 
-                var LEES_POLIS = 'LEES_POLIS';
-                var LIJST_POLISSEN = 'LIJST_POLISSEN';
-                var BEEINDIG_POLIS = 'BEEINDIG_POLIS';
-                var OPSLAAN_POLIS = 'OPSLAAN_POLIS';
-                var VERWIJDER_POLIS = 'VERWIJDER_POLIS';
+                            {naam: 'LEES_POLIS',                        url: BASISURL_RELATIEBEHEER + '/medewerker/polis/lees'},
+                            {naam: 'LIJST_POLISSEN',                    url: BASISURL_RELATIEBEHEER + '/medewerker/polis/lijst'},
+                            {naam: 'BEEINDIG_POLIS',                    url: BASISURL_RELATIEBEHEER + '/medewerker/polis/beeindigen'},
+                            {naam: 'OPSLAAN_POLIS',                     url: BASISURL_RELATIEBEHEER + '/medewerker/polis/opslaan'},
+                            {naam: 'VERWIJDER_POLIS',                   url: BASISURL_RELATIEBEHEER + '/medewerker/polis/verwijder'},
 
-                var VERWIJDER_BIJLAGE = 'VERWIJDER_BIJLAGE';
-                var UPLOAD_BIJLAGE_BIJ_POLIS = 'UPLOAD_BIJLAGE_BIJ_POLIS';
+                            {naam: 'VERWIJDER_BIJLAGE',                 url: BASISURL_RELATIEBEHEER + '/medewerker/bijlage/verwijder'},
+                            {naam: 'UPLOAD_BIJLAGE_BIJ_POLIS',          url: BASISURL_RELATIEBEHEER + '/medewerker/bijlage/uploadPolis1File'},
 
-                var OPSLAAN_SCHADE = 'OPSLAAN_SCHADE';
-                var VERWIJDER_SCHADE = 'VERWIJDER_SCHADE';
-                var LIJST_STATUS_SCHADE = 'LIJST_STATUS_SCHADE';
-                var LEES_SCHADE = 'LEES_SCHADE';
-                var LIJST_SCHADES = 'LIJST_SCHADES';
+                            {naam: 'OPSLAAN_SCHADE',                    url: BASISURL_RELATIEBEHEER + '/medewerker/schade/opslaan'},
+                            {naam: 'VERWIJDER_SCHADE',                  url: BASISURL_RELATIEBEHEER + '/medewerker/schade/verwijder'},
+                            {naam: 'LIJST_STATUS_SCHADE',               url: BASISURL_RELATIEBEHEER + '/medewerker/overig/lijstStatusSchade'},
+                            {naam: 'LEES_SCHADE',                       url: BASISURL_RELATIEBEHEER + '/medewerker/schade/lees'},
+                            {naam: 'LIJST_SCHADES',                     url: BASISURL_RELATIEBEHEER + '/medewerker/schade/lijst'},
 
-                var LIJST_SOORTEN_HYPOTHEEK = 'LIJST_SOORTEN_HYPOTHEEK';
-                var LIJST_HYPOTHEKEN_INCL_PAKKETTEN = 'LIJST_HYPOTHEKEN_INCL_PAKKETTEN';
-                var LEES_HYPOTHEEK = 'LEES_HYPOTHEEK';
-                var LIJST_HYPOTHEKEN = 'LIJST_HYPOTHEKEN';
-                var LIJST_HYPOTHEEKPAKKETTEN = 'LIJST_HYPOTHEEKPAKKETTEN';
-                var OPSLAAN_HYPOTHEEK = 'OPSLAAN_HYPOTHEEK';
-                var VERWIJDER_HYPOTHEEK = 'VERWIJDER_HYPOTHEEK';
+                            {naam: 'LIJST_SOORTEN_HYPOTHEEK',           url: BASISURL_RELATIEBEHEER + '/medewerker/hypotheek/alleSoortenHypotheek'},
+                            {naam: 'LIJST_HYPOTHEKEN_INCL_PAKKETTEN',   url: BASISURL_RELATIEBEHEER + '/medewerker/hypotheek/lijstHypothekenInclDePakketten'},
+                            {naam: 'LEES_HYPOTHEEK',                    url: BASISURL_RELATIEBEHEER + '/medewerker/hypotheek/lees'},
+                            {naam: 'LIJST_HYPOTHEKEN',                  url: BASISURL_RELATIEBEHEER + '/medewerker/hypotheek/lijstHypotheken'},
+                            {naam: 'LIJST_HYPOTHEEKPAKKETTEN',          url: BASISURL_RELATIEBEHEER + '/medewerker/hypotheek/lijstHypotheekPakketten'},
+                            {naam: 'OPSLAAN_HYPOTHEEK',                 url: BASISURL_RELATIEBEHEER + '/medewerker/hypotheek/opslaan'},
+                            {naam: 'VERWIJDER_HYPOTHEEK',               url: BASISURL_RELATIEBEHEER + '/medewerker/hypotheek/verwijder'},
 
-                var LIJST_OPEN_AANGIFTES = 'LIJST_OPEN_AANGIFTES';
-                var LIJST_GESLOTEN_AANGIFTES = 'LIJST_GESLOTEN_AANGIFTES';
-                var AFRONDEN_AANGIFTE = 'AFRONDEN_AANGIFTE';
-                var OPSLAAN_AANGIFTE = 'OPSLAAN_AANGIFTE';
+                            {naam: 'LIJST_OPEN_AANGIFTES',              url: BASISURL_RELATIEBEHEER + '/medewerker/aangifte/openAangiftes'},
+                            {naam: 'LIJST_GESLOTEN_AANGIFTES',          url: BASISURL_RELATIEBEHEER + '/medewerker/aangifte/geslotenAangiftes'},
+                            {naam: 'AFRONDEN_AANGIFTE',                 url: BASISURL_RELATIEBEHEER + '/medewerker/aangifte/afronden'},
+                            {naam: 'OPSLAAN_AANGIFTE',                  url: BASISURL_RELATIEBEHEER + '/medewerker/aangifte/opslaan'},
 
-                var LEES_TAAK = 'LEES_TAAK';
-                var LIJST_TAKEN = 'LIJST_TAKEN';
-                var AFHANDELEN_TAAK = 'AFHANDELEN_TAAK';
-                var VRIJGEVEN_TAAK = 'VRIJGEVEN_TAAK';
-                var OPPAKKEN_TAAK = 'OPPAKKEN_TAAK';
+                            {naam: 'LEES_TAAK',                         url: BASISURL_RELATIEBEHEER + '/medewerker/taak/lees'},
+                            {naam: 'LIJST_TAKEN',                       url: BASISURL_RELATIEBEHEER + '/medewerker/taak/lijst'},
+                            {naam: 'AFHANDELEN_TAAK',                   url: BASISURL_RELATIEBEHEER + '/medewerker/taak/afhandelen'},
+                            {naam: 'VRIJGEVEN_TAAK',                    url: BASISURL_RELATIEBEHEER + '/medewerker/taak/vrijgeven'},
+                            {naam: 'OPPAKKEN_TAAK',                     url: BASISURL_RELATIEBEHEER + '/medewerker/taak/oppakken'},
+                        ];
 
-                if(welkeFunctie == INLOGGEN){
-                    return BASISURL_RELATIEBEHEER + "/authorisatie/authorisatie/inloggen";
-                }else if(welkeFunctie == INGELOGDE_GEBRUIKER){
-                    return BASISURL_RELATIEBEHEER + "/authorisatie/authorisatie/ingelogdeGebruiker";
-                }else if(welkeFunctie == UITLOGGEN){
-                    return BASISURL_RELATIEBEHEER + "/authorisatie/authorisatie/uitloggen";
+                var url = '';
 
-                }else if(welkeFunctie == LIJST_RELATIES){
-                    return BASISURL_RELATIEBEHEER + "/medewerker/gebruiker/zoekOpNaamAdresOfPolisNummer";
-                }else if(welkeFunctie == LEES_RELATIE){
-                    return BASISURL_RELATIEBEHEER + "/medewerker/gebruiker/lees";
-                }else if(welkeFunctie == OPSLAAN_RELATIE){
-                    return BASISURL_RELATIEBEHEER + "/medewerker/gebruiker/opslaan";
-                }else if(welkeFunctie == VERWIJDER_RELATIE){
-                    return BASISURL_RELATIEBEHEER + "/medewerker/gebruiker/verwijderen";
-
-                }else if(welkeFunctie == LIJST_BEDRIJVEN_BIJ_RELATIE){
-                    return BASISURL_RELATIEBEHEER + "/medewerker/bedrijf/lijst";
-                }else if(welkeFunctie == OPSLAAN_BEDRIJF){
-                    return BASISURL_RELATIEBEHEER + "/medewerker/gebruiker/opslaanBedrijf";
-                }else if(welkeFunctie == LEES_BEDRIJF){
-                    return BASISURL_RELATIEBEHEER + "/medewerker/bedrijf/lees";
-
-                }else if(welkeFunctie == LIJST_VERZEKERINGSMAATSCHAPPIJEN){
-                    return BASISURL_RELATIEBEHEER + "/medewerker/overig/lijstVerzekeringsMaatschappijen";
-
-                }else if(welkeFunctie == LEES_POLIS){
-                    return BASISURL_RELATIEBEHEER + "/medewerker/polis/lees";
-                }else if(welkeFunctie == LIJST_POLISSEN){
-                    return BASISURL_RELATIEBEHEER + "/medewerker/polis/lijst";
-                }else if(welkeFunctie == BEEINDIG_POLIS){
-                    return BASISURL_RELATIEBEHEER + "/medewerker/polis/beeindigen";
-                }else if(welkeFunctie == OPSLAAN_POLIS){
-                    return BASISURL_RELATIEBEHEER + "/medewerker/polis/opslaan";
-                }else if(welkeFunctie == VERWIJDER_POLIS){
-                    return BASISURL_RELATIEBEHEER + "/medewerker/polis/verwijder";
-
-                }else if(welkeFunctie == VERWIJDER_BIJLAGE){
-                    return BASISURL_RELATIEBEHEER + "/medewerker/bijlage/verwijder";
-                }else if(welkeFunctie == UPLOAD_BIJLAGE_BIJ_POLIS){
-                    return BASISURL_RELATIEBEHEER + "/medewerker/bijlage/uploadPolis1File";
-
-                }else if(welkeFunctie == OPSLAAN_SCHADE){
-                    return BASISURL_RELATIEBEHEER + "/medewerker/schade/opslaan";
-                }else if(welkeFunctie == VERWIJDER_SCHADE){
-                    return BASISURL_RELATIEBEHEER + "/medewerker/schade/verwijder";
-                }else if(welkeFunctie == LIJST_STATUS_SCHADE){
-                    return BASISURL_RELATIEBEHEER + "/medewerker/overig/lijstStatusSchade";
-                }else if(welkeFunctie == LEES_SCHADE){
-                    return BASISURL_RELATIEBEHEER + "/medewerker/schade/lees";
-                }else if(welkeFunctie == LIJST_SCHADES){
-                    return BASISURL_RELATIEBEHEER + "/medewerker/schade/lijst";
-
-                }else if(welkeFunctie == LIJST_SOORTEN_HYPOTHEEK){
-                    return BASISURL_RELATIEBEHEER + "/medewerker/hypotheek/alleSoortenHypotheek";
-                }else if(welkeFunctie == LIJST_HYPOTHEKEN_INCL_PAKKETTEN){
-                    return BASISURL_RELATIEBEHEER + "/medewerker/hypotheek/lijstHypothekenInclDePakketten";
-                }else if(welkeFunctie == LEES_HYPOTHEEK){
-                    return BASISURL_RELATIEBEHEER + "/medewerker/hypotheek/lees";
-                }else if(welkeFunctie == LIJST_HYPOTHEKEN){
-                    return BASISURL_RELATIEBEHEER + "/medewerker/hypotheek/lijstHypotheken";
-                }else if(welkeFunctie == LIJST_HYPOTHEEKPAKKETTEN){
-                    return BASISURL_RELATIEBEHEER + "/medewerker/hypotheek/lijstHypotheekPakketten";
-                }else if(welkeFunctie == OPSLAAN_HYPOTHEEK){
-                    return BASISURL_RELATIEBEHEER + "/medewerker/hypotheek/opslaan";
-                }else if(welkeFunctie == VERWIJDER_HYPOTHEEK){
-                    return BASISURL_RELATIEBEHEER + "/medewerker/hypotheek/verwijder";
-
-                }else if(welkeFunctie == LIJST_OPEN_AANGIFTES){
-                    return BASISURL_RELATIEBEHEER + "/medewerker/aangifte/openAangiftes";
-                }else if(welkeFunctie == LIJST_GESLOTEN_AANGIFTES){
-                    return BASISURL_RELATIEBEHEER + "/medewerker/aangifte/geslotenAangiftes";
-                }else if(welkeFunctie == AFRONDEN_AANGIFTE){
-                    return BASISURL_RELATIEBEHEER + "/medewerker/aangifte/afronden";
-                }else if(welkeFunctie == OPSLAAN_AANGIFTE){
-                    return BASISURL_RELATIEBEHEER + "/medewerker/aangifte/opslaan";
-
-                }else if(welkeFunctie == LEES_TAAK){
-                    return BASISURL_RELATIEBEHEER + "/medewerker/taak/lees";
-                }else if(welkeFunctie == LIJST_TAKEN){
-                    return BASISURL_RELATIEBEHEER + "/medewerker/taak/lijst";
-                }else if(welkeFunctie == AFHANDELEN_TAAK){
-                    return BASISURL_RELATIEBEHEER + "/medewerker/taak/afhandelen";
-                }else if(welkeFunctie == VRIJGEVEN_TAAK){
-                    return BASISURL_RELATIEBEHEER + "/medewerker/taak/vrijgeven";
-                }else if(welkeFunctie == OPPAKKEN_TAAK){
-                    return BASISURL_RELATIEBEHEER + "/medewerker/taak/oppakken";
+                for (var i = 0; i < vars.length; i++) {
+                    if(welkeFunctie === vars[i].naam) {
+                        url = vars[i].url;
+                        break;
+                    }
                 }
+                log.debug(url);
+
+                return url;
+
             }
         }
     }
