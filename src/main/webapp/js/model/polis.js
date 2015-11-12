45a2e7b1b9daeee2829c12aebb99ca345d98e8e0
Patrick Heidotting <patrick@lakedigital.nl>
2015-11-12 19:58:32 +0100

    DJFC-42 Toevoegen bij Polis verzekerde zaak/object

--- src/main/webapp/js/model/polis.js
+++ src/main/webapp/js/model/polis.js
@@ -59,6 +59,7 @@ define(['jquery',
 		_polis.premie = ko.observable(data.premie);
 		_polis.betaalfrequentie = ko.observable(data.betaalfrequentie);
 		_polis.dekking = ko.observable(data.dekking);
+		_polis.verzekerdeZaak = ko.observable(data.verzekerdeZaak);
 		_polis.bedrijf = ko.observable(data.bedrijf);
 		_polis.omschrijvingVerzekering = ko.observable(data.omschrijvingVerzekering);
 		_polis.idDiv = ko.computed(function() {
