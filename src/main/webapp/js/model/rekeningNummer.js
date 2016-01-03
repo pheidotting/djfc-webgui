50829146d9b351b7c4344faf8ec628441416ceeb
Patrick Heidotting <patrick@lakedigital.nl>
2016-01-03 09:53:57 +0100

    sonar

--- src/main/webapp/js/model/rekeningNummer.js
+++ src/main/webapp/js/model/rekeningNummer.js
@@ -2,13 +2,12 @@ define(['jquery',
         "knockout"],
     function($, ko) {
 
-	return function telefoonnummerModel (data){
-	    thisRek = this;
+	return function(data){
+	    var thisRek = this;
 
 	    thisRek.id = ko.observable(data.id);
         thisRek.rekeningnummer = ko.observable(data.rekeningnummer),
         thisRek.bic = ko.observable(data.bic)
-
 
         thisRek.startBewerken = function(nummer){
             nummer.rekeningnummer(nummer.rekeningnummer().replace(/ /g, ""));
@@ -20,10 +19,10 @@ define(['jquery',
 
 
         zetRekeningnummerOm = function(nummer){
-            if(nummer.rekeningnummer() != undefined){
+            if(nummer.rekeningnummer() !== undefined){
                 var rek = nummer.rekeningnummer().toUpperCase();
 
-                if(rek != undefined && rek.length == 18){
+                if(rek !== undefined && rek.length === 18){
                     rek = rek.substring(0, 4) + " " +rek.substring(4, 8) + " " +rek.substring(8, 12) + " " +rek.substring(12, 16) + " " +rek.substring(16, 18);
                 }
 
