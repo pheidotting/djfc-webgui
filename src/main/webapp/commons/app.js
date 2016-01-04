f6a19a6bad0f7034278121582c3166cbbb72d665
Patrick Heidotting <patrick@lakedigital.nl>
2016-01-04 20:11:39 +0100

    sonar

--- src/main/webapp/commons/app.js
+++ src/main/webapp/commons/app.js
@@ -56,86 +56,86 @@ function   ($, Sammy, commonFunctions, inloggen, lijstRelaties, lijstBedrijven, 
 	var app = new Sammy('body');
 
 	app.route('GET', '#taken', function() {
-		new taken();
+		var a = new taken();
 	});
 	
 	app.route('GET', '#dashboard', function() {
-		new dashboard();
+		var a = new dashboard();
 	});
 	
 	app.route('GET', '#taak/:id', function() {
-		id = this.params['id'];
-		new afhandelenTaak(id);
+		var id = this.params['id'];
+		var a = new afhandelenTaak(id);
 	});
 
 	app.route('GET', '#inloggen', function() {
-		new inloggen();
+		var a = new inloggen();
 	});
 
 	app.route('GET', '#lijstRelaties', function() {
-		new lijstRelaties();
+		var a = new lijstRelaties();
 	});
 
 	app.route('GET', '#lijstRelaties/:zoekTerm', function() {
 		zoekTerm = this.params['zoekTerm'];
-		new lijstRelaties(zoekTerm);
+		var a = new lijstRelaties(zoekTerm);
 	});
 
 	app.route('GET', '#lijstBedrijven', function() {
-		new lijstBedrijven();
+		var a = new lijstBedrijven();
 	});
 
 	app.route('GET', '#lijstBedrijven/:zoekTerm', function() {
-		zoekTerm = this.params['zoekTerm'];
-		new lijstBedrijven(zoekTerm);
+		var zoekTerm = this.params['zoekTerm'];
+		var a = new lijstBedrijven(zoekTerm);
 	});
 
 	app.route('GET', '#beherenBedrijf/:id/:actie/:subId', function() {
-		id = this.params['id'];
-		actie = this.params['actie'];
-		subId = this.params['subId'];
-		new beherenBedrijf(id, actie, subId);
+		var id = this.params['id'];
+		var actie = this.params['actie'];
+		var subId = this.params['subId'];
+		var a = new beherenBedrijf(id, actie, subId);
 	});
 
 	app.route('GET', '#beherenBedrijf/:id/:actie', function() {
-		id = this.params['id'];
-		actie = this.params['actie'];
-		new beherenBedrijf(id, actie);
+		var id = this.params['id'];
+		var actie = this.params['actie'];
+		var a = new beherenBedrijf(id, actie);
 	});
 
 	app.route('GET', '#beherenBedrijf/:id', function() {
-		id = this.params['id'];
-		new beherenBedrijf(id);
+		var id = this.params['id'];
+		var a = new beherenBedrijf(id);
 	});
 
 	app.route('GET', '#beherenBedrijf', function() {
-		new beherenBedrijf(null);
+		var a = new beherenBedrijf(null);
 	});
 
 	app.route('GET', '#beherenRelatie/:id/:actie/:subId', function() {
-		id = this.params['id'];
-		actie = this.params['actie'];
-		subId = this.params['subId'];
-		new beherenRelatie(id, actie, subId);
+		var id = this.params['id'];
+		var actie = this.params['actie'];
+		var subId = this.params['subId'];
+		var a = new beherenRelatie(id, actie, subId);
 	});
 
 	app.route('GET', '#beherenRelatie/:id/:actie', function() {
-		id = this.params['id'];
-		actie = this.params['actie'];
-		new beherenRelatie(id, actie);
+		var id = this.params['id'];
+		var actie = this.params['actie'];
+		var a = new beherenRelatie(id, actie);
 	});
 
 	app.route('GET', '#beherenRelatie/:id', function() {
-		id = this.params['id'];
-		new beherenRelatie(id);
+		var id = this.params['id'];
+		var a = new beherenRelatie(id);
 	});
 
 	app.route('GET', '#beherenRelatie', function() {
-		new beherenRelatie(null);
+		var a = new beherenRelatie(null);
 	});
 
 	app.route('GET', '', function() {
-		new lijstRelaties();
+		var a = new lijstRelaties();
 	});
 
 	app.raise_errors = true;
