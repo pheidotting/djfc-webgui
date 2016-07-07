0f47b7bd7aff921398119e4c68dba7275b38f0f4
Patrick Heidotting <patrick@lakedigital.nl>
2016-07-07 21:26:11 +0200

    Alleen naar de inlogpagina bij een 401 error

--- src/main/webapp/js/commons/dataServices.js
+++ src/main/webapp/js/commons/dataServices.js
@@ -11,7 +11,9 @@ define(["commons/3rdparty/log",
                 $.get(url, data).done(function(response) {
                     return deferred.resolve(response);
                 }).fail(function(response){
-                    redirect.redirect('INLOGGEN');
+                    if (response.status === 401) {
+                        redirect.redirect('INLOGGEN');
+                    }
                     return deferred.reject();
                 });
 
