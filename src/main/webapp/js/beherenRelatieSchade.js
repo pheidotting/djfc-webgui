b978f0b3260d650945336dd2c97cafe4d8b7444d
Patrick Heidotting <patrick@lakedigital.nl>
2016-09-17 19:27:35 +0200

    Geen opmerkingen en fileupload tonen bij nieuwe Schade

--- src/main/webapp/js/beherenRelatieSchade.js
+++ src/main/webapp/js/beherenRelatieSchade.js
@@ -48,12 +48,8 @@ define(['jquery',
 					log.debug("applybindings met een nieuw Schade object");
 					var schade = new Schade('');
 					schade.relatie(relatieId);
-                    fileUpload.init(relatieId).done(function(){
-						new opmerkingenLoader(relatieId).init().done(function(){
-	    					ko.applyBindings(schade);
-        			        $.unblockUI();
-						});
-                    });
+                    ko.applyBindings(schade);
+                    $.unblockUI();
 				}
 			});
 		});
