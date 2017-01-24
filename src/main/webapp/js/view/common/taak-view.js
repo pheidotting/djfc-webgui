97aa6da5e68e6e1b36c1ce7ad4b011ea7469198f
Patrick Heidotting <patrick@lakedigital.nl>
2017-01-24 13:01:06 +0100

    sonar

--- src/main/webapp/js/view/common/taak-view.js
+++ src/main/webapp/js/view/common/taak-view.js
@@ -8,12 +8,10 @@ define(['jquery',
         return {
             init: function(id) {
                 $.when(toggleService.isFeatureBeschikbaar('TODOIST')).then(function(toggleBeschikbaar) {
-                    if(toggleBeschikbaar) {
-                        if(toggleBeschikbaar && id != 0) {
-                            $('#taken').html(html);
+                    if(toggleBeschikbaar && id != 0) {
+                        $('#taken').html(html);
 
-                            logger.debug('content geladen, viewmodel init');
-                        }
+                        logger.debug('content geladen, viewmodel init');
                     }
                 });
             }
